71d2f7768b7eadff3ab05a26e63ebe9b8356b1f6
WeiHung <wei_hung.chin@daimler.com>
2020-11-28 17:32:01 +0800

    Add angular socket app

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  projects: ['<rootDir>/apps/demo-app', '<rootDir>/libs/demo-app/shared-ui'],
+  projects: [
+    '<rootDir>/apps/demo-app',
+    '<rootDir>/libs/demo-app/shared-ui',
+    '<rootDir>/apps/angular-socket',
+  ],
 };
