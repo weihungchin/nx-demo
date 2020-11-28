7b91e283ebb6d7bc6179e94f39199e9e6b764d0d
WeiHung <iforte_intellige.chin@daimler.com>
2020-11-28 19:37:08 +0800

    Add node socket and client socket empty project

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,7 @@ module.exports = {
     '<rootDir>/libs/demo-app/shared-ui',
     '<rootDir>/apps/api',
     '<rootDir>/libs/util-interface',
+    '<rootDir>/apps/angular-socket',
+    '<rootDir>/apps/node-socket',
   ],
 };
